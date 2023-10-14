1622e589d00fa387b4b42bb52a91dc1afb7be22b
AndrewC <aa1371515@gmail.com>
2023-10-14 14:38:47 +0800

    Update samplebot.py

--- samplebot.py
+++ samplebot.py
@@ -32,7 +32,9 @@ B = 1000001
 S = 5000001
 def main():
     args = parse_arguments()
-
+    global B,S
+    B = 1000001
+    S = 5000001
     exchange = ExchangeConnection(args=args)
 
     # Store and print the "hello" message received from the exchange. This
