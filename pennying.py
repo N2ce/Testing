8556a4902f811d6cede0f565a334b8739ae46713
AndrewC <aa1371515@gmail.com>
2023-10-14 15:00:05 +0800

    Update pennying.py

--- pennying.py
+++ pennying.py
@@ -27,9 +27,9 @@ class stockpennying:
     def ordering(self,exchange):
         global B,S
         exchange.send_add_message(order_id=S, symbol=self.symbol, dir="SELL", price=self.bid_price+1, size=self.askvol)
-        print("SOrder sent")
+        print("SOrder sent", self.symbol)
         exchange.send_add_message(order_id=B, symbol=self.symbol, dir="BUY", price=self.ask_price-1, size=self.bidvol)
-        print("BOrder sent")
+        print("BOrder sent", self.symbol)
         self.Sorderid.append(S)
         self.Borderid.append(B)
         S += 1
@@ -40,5 +40,4 @@ class stockpennying:
             del self.Borderid[0]
             del self.Sorderid[0]
             print('canceled')
-        time.sleep(3)
         
