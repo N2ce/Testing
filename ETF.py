cabe46a321163919fa147001dedd417cd628d956
AndrewC <aa1371515@gmail.com>
2023-10-14 16:11:41 +0800

    Update ETF.py

--- ETF.py
+++ ETF.py
@@ -13,12 +13,16 @@ def estimation(message):
         sell_sum_value = sum(sublist[0] for sublist in message["sell"])
         if message["symbol"] == "GS":
             temp[0] = ((buy_sum_value*buy_total)+(sell_total*sell_sum_value))/(buy_total+sell_total)
+            print(temp[0])
         if message["symbol"] == "MS":
             temp[1] = ((buy_sum_value*buy_total)+(sell_total*sell_sum_value))/(buy_total+sell_total)
+            print(temp[1])
         if message["symbol"] == "WFC":
             temp[2] = ((buy_sum_value*buy_total)+(sell_total*sell_sum_value))/(buy_total+sell_total)
+            print(temp[2])
         if message["symbol"] == "XLF":
             temp[3] = ((buy_sum_value*buy_total)+(sell_total*sell_sum_value))//(buy_total+sell_total)
+            print(temp[3])
         print("success")
 
 def weighting(exchange):
